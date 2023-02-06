021a0dbd96eb2e46bd6765008162608d9840c682
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2023-02-06 15:58:11 +0100

    Working example

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -1,25 +1,37 @@
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_sql import SparkSqlOperator
 import requests
-import json
 from config.dag_config import default_args
 
 BASE_URL = "https://strangerthings-quotes.vercel.app/api/quotes"
 
 dag = DAG(dag_id="stranger_things_quotes", default_args=default_args, schedule="@daily")
 
-quote = {}
+query = "SELECT author from stranger_things.quotes"
 
+# fetch new daily quote
 response = requests.get(f"{BASE_URL}")
 if response.status_code == 200:
-    quote = response.json()[0]
-
-query = f"INSERT INTO stranger_things.quotes \
-(quote, author) VALUES ({quote['quote']}, {quote['author']})"
+    quote = response.json()[
+        0
+    ]  # if we get new quote, make query to insert it, otherwise retrieve authors
+    query = f"""
+        INSERT INTO stranger_things.quotes PARTITION(author="{quote['author']}") 
+        VALUES ("{quote['quote']}")
+    """
 
 quotes_job = SparkSqlOperator(
-    sql="SELECT * FROM stranger_things.quotes",
+    sql=query,
     master="local",
     task_id="quotes_job",
     dag=dag,
 )
+
+quotes_count_job = SparkSqlOperator(
+    sql="SELECT COUNT(quote), author FROM stranger_things.quotes GROUP BY author",
+    master="local",
+    task_id="quotes_count_job",
+    dag=dag,
+)
+
+quotes_job >> quotes_count_job
